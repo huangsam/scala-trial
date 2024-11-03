38e3394972f36290d5eef00575338a8cc4bd29e1
Samuel Huang <samhuang91@gmail.com>
2024-11-03 03:28:07 -0800

    Add fibonacci implmentation

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,4 +3,17 @@ package io.huangsam.trial
 @main
 def main(): Unit = {
   println("Hello world!")
+
+  for (i <- 1 to 10) {
+    println(fibonacci(i))
+  }
+}
+
+def fibonacci(n: Int): Int = {
+  if (n < 1) {
+    throw new IllegalArgumentException(s"Invalid input here ${n}");
+  } else if (n < 3) {
+    return n;
+  }
+  fibonacci(n - 1) + fibonacci(n - 2)
 }
