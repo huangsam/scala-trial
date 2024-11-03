d0abba3bbe92d1b58d3c36bdcb2f9d370a8c1609
Samuel Huang <samhuang91@gmail.com>
2024-11-03 04:22:08 -0800

    Add comment and totalSum

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,13 +1,21 @@
 @main
 def main(): Unit = {
+  // Basic I/O
   println("Hello world!")
 
+  // For loop with range
   for (i <- 1 to 10) {
     println(fibonacci(i))
   }
 
+  // Java interoperability
   val human = new Human()
   human.talk()
+
+  // Functional paradigms
+  val numbers = List(1.0, 2.0, 3.0, 4.0, 5.0)
+  val total = totalSum(numbers)
+  println(s"Sum of ${numbers.mkString(" + ")} = $total")
 }
 
 def fibonacci(n: Int): Int = {
@@ -17,4 +25,8 @@ def fibonacci(n: Int): Int = {
     return n
   }
   fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+def totalSum(numbers: List[Double]): Double = {
+  numbers.foldLeft(0.0)(_ + _)
 }
